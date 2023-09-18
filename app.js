d22f43c7ad89bec43baebceb6415a1fc1b6ffa2c
Olawale <105588335+Snotat@users.noreply.github.com>
2023-09-18 10:45:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ let bodyParser = require("body-parser");
 var app = express();
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
 
 require("dotenv").config();
 
@@ -24,13 +22,6 @@ var getRouter = require("./routes/getNote");
 var setRouter = require("./routes/setNote");
 var editRouter = require("./routes/editNote");
 
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
 mongoose.connect(
   "mongodb+srv://olawale:olawale5566@cluster0.cun74.mongodb.net/contact?retryWrites=true&w=majority",
   {
@@ -44,8 +35,6 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function callback() {
   console.log("Database connected successfully");
 });
-
-app.use(connectLiveReload());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -63,19 +52,5 @@ app.use("/set", setRouter);
 app.use("/edit", editRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports = app;
